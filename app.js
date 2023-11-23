78166eaa8e1a5fc31c87b174e1ae542735f13077
imran <muhammadalimran22@gmail.com>
2023-11-23 15:35:33 +0700

    setup init

---
+++ app.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const path = require("path");
+const { PORT = 3000 } = process.env;
+
+const { notFoundHandler, errorHandler } = require("./middlewares");
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "views")));
+
+// server error handling middleware
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log("Running on port", PORT));

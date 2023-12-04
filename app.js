b5a305ddf4eb01ff1e42363221b03c705bddb1e1
dzachmidz20 <fahmialhafidza@gmail.com>
2023-12-04 23:39:55 +0700

    feat: add user for admin

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const morgan = require("morgan");
 const path = require("path");
 const { PORT = 3000 } = process.env;
@@ -9,6 +10,7 @@ const { notFoundHandler, errorHandler } = require("./middlewares");
 
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(cors());
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "views")));
@@ -17,9 +19,6 @@ app.use("/api/v1", require("./routes/index.routes"));
 
 app.use("/api/v1/auth", require("./routes/auth.routes"));
 app.use("/api/v1", require("./routes/category.routes"));
-
-const userRouter = require('./routes/user.routes');
-app.use('/users', userRouter);
 
 // server error handling middleware
 app.use(notFoundHandler);
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const auth = require('./auth.routes');
 const classs = require('./class.routes');
 const user = require('./user.routes')
+const admin = require('./admin.user.routes')
 const { restrict } = require("../middlewares/auth.middlewares");
 
 router.use('/auth', auth);
 router.use('/class', classs);
 router.use('/user', user);
+router.use('/admin', admin);
 
 module.exports = router;

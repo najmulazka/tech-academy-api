028ce94d57429689e74f0826d953db7426edb236
najmulazka <najmulazka225@gmail.com>
2023-12-05 14:36:52 +0700

    delete duplicate variable user

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -6,7 +6,6 @@ const classs = require('./class.routes');
 const categories = require('./category.routes');
 const chapter = require('./chapter.routes');
 const payment = require('./payment.routes');
-const user = require('./user.routes')
 const admin = require('./admin.user.routes')
 const { restrict } = require("../middlewares/auth.middlewares");
 
@@ -17,7 +16,6 @@ router.use('/class', classs);
 router.use('/category', categories);
 router.use('/chapter', chapter);
 router.use('/payment', restrict, payment);
-router.use('/user', user);
 router.use('/admin', admin);
 
 module.exports = router;

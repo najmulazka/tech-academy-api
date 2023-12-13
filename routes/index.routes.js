a1498fe627e2cfd369d95f48d119ffd55ada3f25
najmulazka <najmulazka225@gmail.com>
2023-12-13 20:13:55 +0700

    resolve conflict

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -12,30 +12,12 @@ const notifications = require("./notif.routes");
 const rating = require("./rating.routes");
 const { restrict } = require("../middlewares/auth.middlewares");
 const { isAdmin } = require("../middlewares/admin.midlewares");
-<<<<<<< HEAD
-const lesson = require("./lesson.routes");
-=======
 const lesson = require('./lesson.routes');
 const learning = require('./learning.routes');
->>>>>>> 38ac2bdb105ba906f89a3436f8e3f1207363f07d
 
 // const admin = require('./admin.routes');
 // const { admin } = require('../middlewares/admin.midlewares');
 
-<<<<<<< HEAD
-router.use("/auth", auth);
-router.use("/user", user);
-router.use("/password", password);
-router.use("/class", classs);
-router.use("/category", categories);
-router.use("/chapter", chapter);
-router.use("/payment", restrict, payment);
-router.use("/admin/payment", isAdmin, paymentAdmin);
-router.use("/admin", admin);
-router.use("/notifications", restrict, notifications);
-router.use("/lesson", lesson);
-router.use("/rating", restrict, rating);
-=======
 router.use('/auth', auth);
 router.use('/user', user);
 router.use('/password', password);
@@ -47,7 +29,7 @@ router.use('/admin/payment', isAdmin, paymentAdmin);
 router.use('/admin', admin);
 router.use('/notifications', restrict, notifications)
 router.use('/lesson', lesson)
+router.use("/rating", restrict, rating);
 router.use('/learning', learning)
->>>>>>> 38ac2bdb105ba906f89a3436f8e3f1207363f07d
 
 module.exports = router;

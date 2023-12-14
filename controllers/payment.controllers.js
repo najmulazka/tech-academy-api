06c95a651c7efa9168132fd9ecfd2eb782a88981
najmulazka <najmulazka225@gmail.com>
2023-12-14 11:35:36 +0700

    feat: payment

--- controllers/payment.controllers.js
+++ controllers/payment.controllers.js
@@ -1,16 +1,17 @@
-const prisma = require("../utils/libs/prisma.libs");
+const prisma = require('../utils/libs/prisma.libs');
+const imagekit = require('../utils/libs/imagekit.libs');
+const path = require('path');
 
 const createTransaction = async (req, res, next) => {
   try {
     let { classCode } = req.params;
-    let { paymentMethod, cardNumber } = req.body;
 
     const classExist = await prisma.class.findUnique({ where: { classCode } });
     if (!classExist) {
       return res.status(404).json({
         status: false,
-        message: "Bad Request!",
-        err: "Not Found",
+        message: 'Bad Request!',
+        err: 'Not Found',
         data: null,
       });
     }
@@ -18,8 +19,6 @@ const createTransaction = async (req, res, next) => {
     const transaction = await prisma.transactions.create({
       data: {
         userId: req.user.id,
-        paymentMethod,
-        cardNumber,
         classCode,
       },
     });
@@ -33,7 +32,64 @@ const createTransaction = async (req, res, next) => {
 
     res.status(200).json({
       status: true,
-      message: "OK!",
+      message: 'OK!',
+      err: null,
+      data: transaction,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const paymentWithBank = async (req, res, next) => {
+  try {
+    let { id } = req.params;
+    let { paymentMethod, bankType, struckPicture } = req.body;
+
+    if (!paymentMethod) {
+      return res.status(400).json({ status: false, message: 'Bad Request!', err: 'payment method is require', data: null });
+    }
+
+    if (!bankType) {
+      return res.status(400).json({ status: false, message: 'Bad Request!', err: 'bank type is require', data: null });
+    }
+
+    if (!struckPicture) {
+      return res.status(400).json({ status: false, message: 'Bad Request!', err: 'struck picture is require', data: null });
+    }
+
+    if (!req.file) {
+      return res.status(400).json({ status: false, message: 'Bad Request!', err: 'file is require', data: null });
+    }
+
+    let strFile = req.file.buffer.toString('base64');
+    const transactionExist = await prisma.transactions.findUnique({ where: { id } });
+    if (!transactionExist) {
+      return res.status(404).json({
+        status: false,
+        message: 'Bad Request!',
+        err: 'Not Found',
+        data: null,
+      });
+    }
+
+    const transaction = await prisma.transactions.create({
+      data: {
+        userId: req.user.id,
+        classCode,
+      },
+    });
+
+    if (!classExist.isFree) {
+      await prisma.class.update({
+        where: { classCode },
+        data: { isFree: true },
+      });
+    }
+
+    res.status(200).json({
+      status: true,
+      message: 'OK!',
       err: null,
       data: transaction,
     });
@@ -52,7 +108,7 @@ const getTransactions = async (req, res, next) => {
 
     res.status(200).json({
       status: true,
-      message: "OK!",
+      message: 'OK!',
       data: userTransactions,
     });
   } catch (err) {
@@ -72,15 +128,15 @@ const getDetailTransaction = async (req, res, next) => {
     if (!transaction) {
       return res.status(404).json({
         status: false,
-        message: "Bad Request!",
-        err: "Not Found",
+        message: 'Bad Request!',
+        err: 'Not Found',
         data: null,
       });
     }
 
     res.status(200).json({
       status: true,
-      message: "OK!",
+      message: 'OK!',
       err: null,
       data: transaction,
     });

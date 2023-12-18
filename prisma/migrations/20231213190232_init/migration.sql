77d725ad4b8810ceb45e85b28273e1a034fbc80c
dzachmidz20 <fahmialhafidza@gmail.com>
2023-12-18 21:22:18 +0700

    fix error migrate

--- prisma/migrations/20231213190232_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Learning" ADD COLUMN     "prevPresentase" INTEGER NOT NULL DEFAULT 0;
--- prisma/migrations/20231215124828_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Chapters" ADD COLUMN     "isView" BOOLEAN NOT NULL DEFAULT false;
